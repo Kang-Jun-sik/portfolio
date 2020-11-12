3adf4ead5df08fc4731f14d021a2c8747de37494
강준식 <kjs1436@douzone.com>
2020-11-12 12:05:14 +0900

    - vuetify 적용

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    outputDir: './docs',
-    publicPath: '/portfolio/',
-    configureWebpack: {
-        optimization: {
-            splitChunks: false
-        }
-    },
-    filenameHashing: false,
-    chainWebpack: config => config.optimization.minimize(false), //개발도구 IDE에서 디버깅해야 하는 경우가 생겨 js 파일의 경우는 Uglify false로 수정
+  "outputDir": "./docs",
+  "publicPath": "/portfolio/",
+  "configureWebpack": {
+    "optimization": {
+      "splitChunks": false
+    }
+  },
+  "filenameHashing": false,
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
